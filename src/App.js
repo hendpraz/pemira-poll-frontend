0b9eb12ce9125d5d3943191277ad1a1bb83ecf8e
Fadhil Muhammad Rafi <fadhilrafi90@gmail.com>
2020-12-28 13:33:24 +0700

    add basic router

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import Home from './pages/Home'
 import LoginSelect from './pages/Login/LoginSelect'
 import NonINA from './pages/Login/NonINA'
 import Lembaga from './pages/Login/Lembaga'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Lembaga />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={() => <Home />} />
+        <Route exact path="/login" render={() => <LoginSelect />} />
+        <Route exact path="/login/lembaga" render={() => <Lembaga />} />
+        <Route exact path="/login/nonina" render={() => <NonINA />} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
