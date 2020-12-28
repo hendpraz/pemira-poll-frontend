de64b9fadb270dac0378af826f28623c9e0583b3
Fadhil Muhammad Rafi <fadhilrafi90@gmail.com>
2020-12-28 13:23:07 +0700

    add login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from './pages/Home'
 import LoginSelect from './pages/Login/LoginSelect'
+import NonINA from './pages/Login/NonINA'
+import Lembaga from './pages/Login/Lembaga'
 
 function App() {
   return (
-    <Home />
+    <Lembaga />
   );
 }
 
---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from '../Button'
+import '../../styles/LoginForm.scss'
+
+const LoginForm = () => {
+    return (
+        <div className="login-container">
+            <input type="text" name="username" id="username" placeholder="Username" /> <br />
+            <input type="password" name="password" id="password"  placeholder="Password" /> <br />
+            <div className="btn-container">
+                <Button name="Login" file="Tombol 3"></Button>
+            </div>
+           
+        </div>
+    )
+}
+
+export default LoginForm
