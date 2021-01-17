a2db2c251bd66b3d86f37d2bed60471277c16686
admin <admin@pemira.km>
2021-01-17 13:23:41 +0000

    Update config

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 const config = {
-  defaultURL: "https://localhost:3000",
-  defaultPublicURL: "http://localhost:3000",
-  defaultAPIURL: "http://localhost:8000/api",
-  // defaultURL: "https://demoku.site",
-  // defaultPublicURL: "https://demoku.site",
-  // defaultAPIURL: "https://api.demoku.site/v1",
+  // defaultURL: "https://localhost:3000",
+  // defaultPublicURL: "http://localhost:3000",
+  // defaultAPIURL: "http://localhost:8000/api",
+  defaultURL: "http://167.205.57.53:3000",
+  defaultPublicURL: "http://167.205.57.53:3000",
+  defaultAPIURL: "http://167.205.57.53:8000/api",
 
   assetsURL: {
     image: "/assets/images"
