fc524acfb8d6af1c32ce3b498acba688875da24e
Muhammad Hendry Prasetya <13517105@std.stei.itb.ac.id>
2021-01-25 16:50:37 +0700

    Integrate dukungan massa

--- src/pages/Dukungan/index.js
+++ src/pages/Dukungan/index.js
@@ -4,14 +4,14 @@ import Footer from 'components/Footer'
 import DarkBlue from 'layouts/DarkBlue'
 import Button from 'components/Button'
 import Authenticated from 'layouts/Authenticated'
+import { listKandidatDukungan, dukungKandidat } from 'resources/user'
 import { useAppContext } from 'libs/contextLib'
 
 import '../../styles/pages/Dukungan.scss'
 
 const LembarDukungan = (props) => {
 
-    const [support1, setSupport1] = useState(true)
-    const [support2, setSupport2] = useState(true)
+    const [candidates, setCandidates] = useState([])
 
     const {user} = useAppContext()
     const [pageUser, setPageUser] = useState({})
@@ -22,30 +22,52 @@ const LembarDukungan = (props) => {
         }
     }, [user])
 
-    const handleSupport1 = (e) =>{
-        handleSubmit(e,"dukungan1");
-        setSupport1(false);
-    }
+    useEffect(() => {
+        async function loadCandidates() {
+            try {
+                let response = await listKandidatDukungan()                
+                setCandidates(response)
+            } catch (e) {
+                console.log(e)
+            }
+        }
 
-    const handleSupport2 = (e) =>{
-        handleSubmit(e,"dukungan2");
-        setSupport2(false);
-    }
+        async function onLoad() {
+            loadCandidates()
+        }
 
-    const handleSubmit = (e, formname) =>{
+        onLoad()
+    }, [])
+
+    const handleSubmit = async (e, index) =>{
         e.preventDefault();
-        const form = document.forms[formname];
-        let scriptURL = "https://script.google.com/macros/s/AKfycbxesL1pljDg4e9XODi21hDMumP7vfKN1cj7KtsxbnMZgmKrAaQTLOkH/exec";
-        const body = new FormData(form)
-        fetch(scriptURL,
-            {
-                method: 'POST',
-                body
-            })
-            .then(response => console.log('Success!', response))
-            .catch(error => console.error('Error!', error.message));
-        alert("Berhasil beri dukungan");
-        form.reset();
+        const candidate = candidates[index]
+        const r = window.confirm(`Apakah Anda yakin akan memberi dukungan untuk kandidat ${candidate.fullname}?`)
+        if (r) {
+            const formname = `dukungan-${index}`
+            const form = document.forms[formname]
+            let scriptURL = "https://script.google.com/macros/s/AKfycbxesL1pljDg4e9XODi21hDMumP7vfKN1cj7KtsxbnMZgmKrAaQTLOkH/exec"
+            const body = new FormData(form)
+    
+    
+            // for (var pair of body.entries()) {
+            //     console.log(pair[0]+ ', ' + pair[1]); 
+            // }
+            await fetch(scriptURL,
+                {
+                    method: 'POST',
+                    body
+                })
+                .then(response => console.log('Success!', response))
+                .catch(error => console.error('Error!', error.message))
+    
+            const res = await dukungKandidat(candidate.id)
+            console.log(res)
+            
+            alert("Berhasil beri dukungan")
+            form.reset()
+            window.location.reload()
+        }
     }
 
     return(
@@ -56,32 +78,34 @@ const LembarDukungan = (props) => {
                     <NavMain logo={false}/>
                     <h1 className="title-dukungan">Lembar Dukungan</h1>
                     <ul>
-                        <li>
-                            <form name="dukungan1">
-                                <div>
-                                    <h3>Trian Verson - Fisika 17</h3> 
-                                    <div className="flex-right">
-                                        {support1 && <Button file="dukung-btn" onClick={handleSupport1}/>}
-                                    </div>
-                                </div>
-                                <input type="hidden" name="name" value={pageUser.fullname}/>
-                                <input type="hidden" name="nim" value={pageUser.nim}/>
-                                <input type="hidden" name="dukung" value="Trian Verson - Fisika 17"/>
-                            </form>
-                        </li>
-                        <li>
-                            <form name="dukungan2">
-                                <div>
-                                    <h3>Nama Calon Kedua</h3> 
-                                    <div className="flex-right">
-                                        {support2 && <Button file="dukung-btn" onClick={handleSupport2}/>}
-                                    </div>
-                                </div>
-                                <input type="hidden" name="name" value={pageUser.fullname}/>
-                                <input type="hidden" name="nim" value={pageUser.nim}/>
-                                <input type="hidden" name="dukung" value="Kandidat 2"/>
-                            </form>
-                        </li>
+                        {
+                            candidates && pageUser
+                            ? <>
+                                {
+                                    candidates.map((item, index) => {
+                                        return (
+                                        <li key={`list-dukungan-${index}`}>
+                                            <form name={`dukungan-${index}`} >
+                                                <div>
+                                                    <h3>{item.fullname + ' - ' + item.ou}</h3> 
+                                                    <div className="flex-right">
+                                                        { item.is_supported 
+                                                        ? <p>Telah Anda dukung.</p>
+                                                        : <Button file="dukung-btn" onClick={e => handleSubmit(e, index)}/>
+                                                        }
+                                                    </div>
+                                                </div>
+                                                <input type="hidden" name="name" value={pageUser.fullname}/>
+                                                <input type="hidden" name="nim" value={pageUser.nim}/>
+                                                <input type="hidden" name="dukung" value={item.fullname + ' - ' + item.ou}/>
+                                            </form>
+                                        </li>
+                                        )
+                                    })
+                                }
+                            </>
+                            : <p>Tidak ada kandidat.</p>
+                        }
                     </ul>
                 </div>
                <Footer hashtag="false" />
--- src/resources/user.js
+++ src/resources/user.js
@@ -59,3 +59,45 @@ export const createKandidat = async (data) =>
       reject(e);
     }
   });
+
+export const listKandidatDukungan = async () =>
+  new Promise(async (resolve, reject) => {    
+    try {
+      const token = localStorage.getItem('token')
+      let response = await fetch(`${defaultAPIURL}/list-kandidat-dukungan/`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        },
+      })
+      response = response.json()
+      console.log(response)
+
+      resolve(response);
+    } catch (e) {
+      reject(e);
+    }
+  });
+
+export const dukungKandidat = (kandidatId) =>
+  new Promise(async (resolve, reject) => {    
+    try {
+      const token = localStorage.getItem('token')
+      let response = await fetch(`${defaultAPIURL}/dukungan-massa/`, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          kandidat: kandidatId
+        })
+      })
+      response = response.json()
+      console.log(response)
+
+      resolve(response);
+    } catch (e) {
+      reject(e);
+    }
+  });
