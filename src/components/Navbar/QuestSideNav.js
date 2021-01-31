a77b1669f8d0fa822c6003cb81b13d2ae15cc5c4
Muhammad Hendry Prasetya <13517105@std.stei.itb.ac.id>
2021-01-31 17:55:49 +0700

    Update sidebar nav

--- src/components/Navbar/QuestSideNav.js
+++ src/components/Navbar/QuestSideNav.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import config from 'config'
 import 'styles/pages/Quest.scss'
-// import {useHistory} from 'react-router-dom'
+import { useAppContext } from 'libs/contextLib'
+import {useHistory} from 'react-router-dom'
 
 const QuestSideNav = ({nav, setNav}) => {
 
-    // const history = useHistory()
+    const history = useHistory()
+    const { user } = useAppContext()
     const {assetsURL: {
             image
         }} = config
@@ -28,46 +30,103 @@ const QuestSideNav = ({nav, setNav}) => {
             .add("active")
     }
 
+    const [pageUser, setPageUser] = useState({})
+
+    useEffect(() => {
+        if (user) {
+            setPageUser(user)
+        }
+    }, [user])
+
+    const defaultContent = () => {
+        return (
+            <>
+                <li>
+                    <div className="home" onClick={() => clickNav("home")}>Home</div>
+                </li>
+                <li>
+                    <div>-</div>
+                </li>
+                <li>
+                    <div>-</div>
+                </li>
+                <li>
+                    <div>-</div>
+                </li>
+                <li>
+                    <div>-</div>
+                </li>
+                <li>
+                    <div>-</div>
+                </li>
+            </>
+        )
+    }
+
+    const getSideNavContent = () => {
+        if (pageUser) {
+            switch (pageUser.groups) {
+                case 3:
+                    return (
+                        <>
+                            <li>
+                                <div className="home" onClick={() => clickNav("home")}>Home</div>
+                            </li>
+                            <li>
+                                <div className="vote-k3m" onClick={() => history.push("/vote-k3m")}>Pemilihan K3M</div>
+                            </li>
+                            <li>
+                                <div className="vote-k3m" onClick={() => history.push("/vote-k3m")}>Pemilihan MWAWM</div>
+                            </li>
+                            <li>
+                                <div>-</div>
+                            </li>
+                            <li>
+                                <div>-</div>
+                            </li>
+                            <li>
+                                <div>-</div>
+                            </li>
+                        </>
+                    )
+                case 4:
+                    return defaultContent()
+                case 5:
+                    return (
+                        <>
+                            <li>
+                                <div className="quest-wall" onClick={() => clickNav("quest-wall")}>Quest Wall</div>
+                            </li>
+                            <li>
+                                <div className="duel-wall" onClick={() => clickNav("duel-wall")}>Duel Wall</div>
+                            </li>
+                            <li>
+                                <div className="tim-sukses" onClick={() => clickNav("tim-sukses")}>Tim Sukses</div>
+                            </li>
+                            <li>
+                                <div className="algojo" onClick={() => clickNav("algojo")}>Algojo</div>
+                            </li>
+                            <li>
+                                <div className="hasil-pemira" onClick={() => clickNav("hasil-pemira")}>Hasil Pemira</div>
+                            </li>
+                        </>
+                    )
+                default:
+                    return defaultContent()
+            }
+        } else {
+            return defaultContent()
+        }
+    }
 
 return (
     <div
         className="quest-side is-hidden-mobile"
         style={{
-        backgroundImage: `url(${image}/sidenavquest.png)`
-    }}>
-        <li>
-            <div className="home" onClick={() => clickNav("home")}>Home</div>
-        </li>
-        <li>
-            <div>-</div>
-        </li>
-        <li>
-            <div>-</div>
-        </li>
-        <li>
-            <div>-</div>
-        </li>
-        <li>
-            <div>-</div>
-        </li>
-        <li>
-            <div>-</div>
-        </li>
-        {/* <li>
-            <div className="quest-wall" onClick={() => clickNav("quest-wall")}>Quest Wall</div>
-        </li>
-        <li>
-            <div className="duel-wall" onClick={() => clickNav("duel-wall")}>Duel Wall</div>
-        </li>
-        <li>
-            <div className="tim-sukses" onClick={() => clickNav("tim-sukses")}>Tim Sukses</div>
-        </li>
-        <li>
-            <div className="algojo" onClick={() => clickNav("algojo")}>Algojo</div>
-        </li>
-        <li>
-            <div className="hasil-pemira" onClick={() => clickNav("hasil-pemira")}>Hasil Pemira</div>
-        </li> */}
+            backgroundImage: `url(${image}/sidenavquest.png)`
+        }}
+    >
+        {getSideNavContent()}
     </div>
 )
 }
