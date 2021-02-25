af6e81423df94eec83a88bc1a1b6c7ae23ec1423
Muhammad Hendry Prasetya <13517105@std.stei.itb.ac.id>
2021-02-25 11:48:44 +0700

    update menu admin page

--- src/pages/AdminPage/Menu.js
+++ src/pages/AdminPage/Menu.js
@@ -12,12 +12,12 @@ const Admin = () => {
                 <div className="container">
                     <h1>Menu Admin</h1>
                     <div class="columns is-variable is-6 py-32">
-                        <div class="column">
+                        <div class="column menu-column">
                             <Link className="menu-item" to="/admin/users">
                                 Manage Akun User
                             </Link>
                         </div>
-                        <div class="column">
+                        <div class="column menu-column">
                             <Link className="menu-item" to="/admin/massa-lembaga">
                                 Hubungkan Massa dan Lembaga
                             </Link>
@@ -25,38 +25,45 @@ const Admin = () => {
                     </div>
 
                     <div class="columns is-vcentered p-16">
-                        <div class="column">
+                        <div class="column menu-column">
                             <Link className="menu-item" to="/admin/bakal-calon">
                                 Manage Data Bakal Calon
                             </Link>
                         </div>
-                        <div class="column">
+                        <div class="column menu-column">
+                            <Link className="menu-item" to="/admin/user-activity">
+                                Lihat Aktivitas User
+                            </Link>
+                        </div>
+                    </div>
+
+                    <div class="columns is-vcentered p-16">
+                        <div class="column menu-column">
                             <Link className="menu-item" to="/admin/questions">
                                 Manage Pertanyaan dan Jawaban
                             </Link>
                         </div>
-                    </div>
-
-                    <div class="columns is-vcentered p-16">
-                        <div class="column">
+                        <div class="column menu-column">
                             <Link className="menu-item" to="/admin/quests">
                                 Manage Quest
                             </Link>
                         </div>
-                        <div class="column">
+                    </div>
+
+                    <div class="columns is-vcentered p-16">
+                        <div class="column menu-column">
                             <Link className="menu-item" to="/admin/quest-proofs">
                                 Quest Proofs
                             </Link>
                         </div>
-                    </div>
-
-                    <div class="columns is-vcentered p-16">
-                    <div class="column">
+                        <div class="column menu-column">
                             <Link className="menu-item" to="/admin/quest-completion">
                                 Quest Completion
                             </Link>
                         </div>
-                        <div class="column">
+                    </div>
+                    <div class="columns is-vcentered p-16">
+                        <div class="column menu-column">
                             <Link className="menu-item" to="/admin/duels">
                                 Manage Duel
                             </Link>
