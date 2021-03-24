006f7a85b360320bb73caf4e4e7378f609b167da
Muhammad Hendry Prasetya <13517105@std.stei.itb.ac.id>
2021-03-24 13:07:42 +0700

    update navbar sop

--- src/pages/GiftShop/index.js
+++ src/pages/GiftShop/index.js
@@ -5,40 +5,50 @@ import "styles/pages/GiftShop.scss";
 import "styles/pages/Home.scss";
 
 //Components
-import Navbar from "components/Navbar/NavMain";
+import NavMain from 'components/Navbar/NavMain.js'
+import NavLoggedIn from 'components/Navbar/NavLoggedIn.js'
 import BarangMenarik from "components/BarangMenarik";
 import TopFive from "pages/Home/TopFive";
 import Footer from "components/Footer";
 
 //Assets
 import config from "config";
+import Authenticated from "layouts/Authenticated";
 
 const GiftShop = () => {
     const {
         assetsURL: { image },
     } = config;
 
+    const isAuthenticated = localStorage.getItem('token')
+    ? true
+    : false
+
     return (
-        <div className="gift-shop-page">
-            <Navbar />
-            <div className="container-kematian mt-5">
-                <div className="border-luar-item">
-                    <div className="border-luar-kuning">
-                        <div className="border-dalem-item">
-                            <div className="border-dalem-kuning">
-                                <div className="border-dalem-item contentnyacok">
-                                    <div className="ads"></div>
-                                    <div className="judul-container">
-                                        <h2
-                                            className="has-text-centered"
-                                            id="Top5"
-                                        >
-                                            Barang-barang menarik
-                                        </h2>
-                                    </div>
-                                    <div className="content-container ct2">
-                                        <div className="barang-menarik">
-                                            <BarangMenarik />
+        <Authenticated>
+            <div className="gift-shop-page">
+                {isAuthenticated
+                ? <NavLoggedIn />
+                : <NavMain />}
+                <div className="container-kematian mt-5">
+                    <div className="border-luar-item">
+                        <div className="border-luar-kuning">
+                            <div className="border-dalem-item">
+                                <div className="border-dalem-kuning">
+                                    <div className="border-dalem-item contentnyacok">
+                                        <div className="ads"></div>
+                                        <div className="judul-container">
+                                            <h2
+                                                className="has-text-centered"
+                                                id="Top5"
+                                            >
+                                                Barang-barang menarik
+                                            </h2>
+                                        </div>
+                                        <div className="content-container ct2">
+                                            <div className="barang-menarik">
+                                                <BarangMenarik />
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -46,10 +56,10 @@ const GiftShop = () => {
                         </div>
                     </div>
                 </div>
+                <TopFive />
+                <Footer />
             </div>
-            <TopFive />
-            <Footer />
-        </div>
+        </Authenticated>
     );
 };
 
